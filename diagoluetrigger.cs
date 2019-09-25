37c361312efaffcc7ac929e398c819baa55effa4
ihmlf1 <53151723+ihmlf1@users.noreply.github.com>
2019-09-25 21:57:28 +0800

    Add files via upload

---
+++ diagoluetrigger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class diagoluetrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+    private Queue<string> sentences = new Queue<string>();
+    void Start()
+    {
+        Debug.Log("this is" + dialogue.name + " speaking");
+        sentences.Clear();
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count==0)
+        {
+            EndDialogue();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        Debug.Log(sentence);
+    }
+    void EndDialogue()
+    {
+        Debug.Log("");
+    }
+    public void TriggerDialogue(Dialogue dialogue)
+    {
+        FindObjectOfType<diagoluemanager>().Stardialogue(dialogue);
+    }
+}
